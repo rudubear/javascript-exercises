bbd6654395cab813e1d0ac65ea40deb687378bfd
RuduBear <134881527+rudubear@users.noreply.github.com>
2023-08-02 18:42:29 -0400

    assignment 02 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, totalReps) {
+    let repeatedString = "";
+    if (Number.isInteger(totalReps) && totalReps >= 0){
+        for(rep = 0; rep < totalReps; rep++){
+            repeatedString += String(str);
+        }
+        return repeatedString;
+    }
+    else return "ERROR";
 };
 
 // Do not edit below this line
