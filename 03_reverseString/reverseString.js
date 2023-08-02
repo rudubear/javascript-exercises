b0529175a9e4fc9733da59deb90c008a46922a9a
RuduBear <134881527+rudubear@users.noreply.github.com>
2023-08-02 19:02:15 -0400

    assignment 03 complete but my solution is overly complicated

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strAry = Array.from(str);
+    let reversedAry = [];
+    for(const letter of strAry) {
+        reversedAry.unshift(letter);
+    }
+    let reversedStr = reversedAry.join("");
+    return reversedStr;
 };
 
 // Do not edit below this line
