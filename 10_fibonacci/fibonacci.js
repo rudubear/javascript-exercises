cfc7b86ca4cb95c0c2220247f364c39c8332fea6
RuduBear <134881527+rudubear@users.noreply.github.com>
2023-08-03 18:55:58 -0400

    assignment 10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
-
+const fibonacci = function(num1) {
+    let reqNum = Number(num1).valueOf();
+    console.log(reqNum);
+    if (reqNum <= 0) {
+        return "OOPS";
+    }
+    else if (reqNum === 1 || reqNum === 2) {
+        return 1;
+    }
+    else {
+        let prevValue = 1;
+        let prevPrevValue = 1;
+        let retValue; 
+        for(let i = 3; i <= reqNum; i ++){
+            retValue = prevPrevValue + prevValue;
+            prevPrevValue = prevValue;
+            prevValue = retValue;
+            console.log("retValue " + retValue +", prevValue" + prevValue + ", prevPrevValue", + prevPrevValue);
+        }
+        return retValue;
+    }
+    return 0;
 };
 
 // Do not edit below this line
