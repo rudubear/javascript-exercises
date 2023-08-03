7dc08ba0362618b0abc3b4a9042df16d310f0cf0
RuduBear <134881527+rudubear@users.noreply.github.com>
2023-08-03 01:54:23 -0400

    assignment 04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(ary, ...args) {
+    const myArray = Array.from(ary);
+    console.log(myArray);
+    for(const arg of Array.from(args)) {
+        console.log(arg);      
+        for(let i = 0; i < myArray.length; i++) {
+            if (myArray[i] === arg) {
+                myArray.splice(i,1);
+                console.log(myArray);
+            } 
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
