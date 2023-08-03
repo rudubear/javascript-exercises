ae5a2474598987419e39c60d1fb2e6981df07b61
RuduBear <134881527+rudubear@users.noreply.github.com>
2023-08-03 01:11:26 -0400

    assignment 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let value = Number((fahrenheit - 32 ) * 5 / 9 ).toFixed(1);
+
+  if (value == 0) return 0;
+  if (value % 1 == 0) {
+    return Number((Number(value).toFixed(0)));
+  }
+  else {
+    return Number(value);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let value = Number(celsius * 9 / 5 + 32).toFixed(1);
+
+  if (value == 0) return 0;
+  if (value % 1 == 0) {
+    return Number((Number(value).toFixed(0)));
+  }
+  else {
+    return Number(value);
+  }
 };
 
 // Do not edit below this line
