677a8aeaa689406cd6e4455566b2f284317a12fe
RuduBear <134881527+rudubear@users.noreply.github.com>
2023-08-03 18:30:38 -0400

    complete assignment 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    console.log(str);
+    const punctuation = ['!','@','#','$','%','^','&','*','(',')','_','/','?','.',' ',','];
+    for(const punct of punctuation) {
+        str = String(str).toLowerCase().replaceAll(punct,'');
+    }
+    console.log(str);
+
+    let isPalindrome = true;
+    for(let i = 0; i < str.length; i++) {
+        if (str[i] !== str[str.length-1-i])
+            isPalindrome = false;
+    }
+    console.log(isPalindrome);
+    return isPalindrome;
 
 };
 
