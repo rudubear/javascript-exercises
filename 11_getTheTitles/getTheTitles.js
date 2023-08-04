b2df2cab4f19d2cb93ef105f1ac495a8d9e85de0
RuduBear <134881527+rudubear@users.noreply.github.com>
2023-08-03 23:45:29 -0400

    assignment 11 complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = books.map((book) => {return book.title});
+    console.log(books);
+    console.log(titles);
+    return titles;
 };
 
 // Do not edit below this line
