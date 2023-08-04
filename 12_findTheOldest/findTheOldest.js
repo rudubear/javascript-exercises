346ee3dc8c9276ef7480225873a6353800af688d
RuduBear <134881527+rudubear@users.noreply.github.com>
2023-08-04 01:47:45 -0400

    assignment 12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleWithDates) {
+    //console.log(peopleWithDates);
+    const currentYear = (new Date()).getFullYear();
+    //console.log(currentYear);
+    const peopleWithAges = peopleWithDates.map(
+        ({name, yearOfBirth, yearOfDeath}) => 
+        ({
+            name: name.valueOf(), 
+            age: yearOfDeath ? yearOfDeath - yearOfBirth : currentYear - yearOfBirth,
+        }));
+    console.log(peopleWithAges);
 
+    let agesSorted = peopleWithAges.sort(function(a, b) {
+        if (a.age > b.age) {
+            return -1;
+        }
+        if (a.age < b.age) {
+            return 1;
+        }
+        return 0;
+    });
+    
+    return agesSorted[0];
 };
 
 // Do not edit below this line
